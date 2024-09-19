eebdcad2f017aa81ebefaede88e065af9bd19c31
rgcbdigital <cheysser@gmail.com>
2024-09-19 18:27:52 +0800

    added while loop, removed unnecessary lines and added exit menu

--- main.rb
+++ main.rb
@@ -2,21 +2,15 @@ require_relative 'student'
 
 def new_student
   puts "Add new student"
-
   student_id = Student.all.size + 1
-
   puts "Enter student name:"
   student_name = gets.chomp
-
   puts "Enter student birth date (YYYY-MM-DD):"
   student_birth_date = gets.chomp
-
   puts "Enter student email:"
   student_email = gets.chomp
-
   puts "Enter student phone number:"
   student_phone_number = gets.chomp
-
   student = Student.new(student_id, student_name, student_birth_date, student_email, student_phone_number)
   student.save
 
@@ -30,29 +24,23 @@ end
 
 def delete_student
   puts "Delete a student"
-
-  # Prompt for student ID
   puts "Enter student ID to delete:"
   student_id = gets.chomp.to_i
 
-  # Find the student by ID
-  student = Student.find(student_id)
-
-  if student
+  if student = Student.find(student_id)
     student.destroy
-    # Confirm student's successful deletion
-    unless Student.find(student_id)
-      puts "Student destroyed successfully!"
-    end
+    puts "Student destroyed successfully!"
   else
     puts "Student with ID #{student_id} not found."
   end
 end
 
 def main_menu
+  while true
     puts "Choose an action:"
     puts "1. Add a new student"
     puts "2. Delete a student"
+    puts "3. Exit"
     choice = gets.chomp.to_i
 
     case choice
@@ -60,9 +48,13 @@ def main_menu
       new_student
     when 2
       delete_student
+    when 3
+      puts "Exiting..."
+      break
     else
-      puts "Invalid choice, please try again."
+      puts "Something seems to be wrong, please try again."
     end
+  end
 end
 
 main_menu
